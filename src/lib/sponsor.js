02b9d87c5c1071f2fa5d4668336868327ad2bd63
rafszn <yhrapha@gmail.com>
2025-09-13 10:26:38 +0100

    exhibitor

--- src/lib/sponsor.js
+++ src/lib/sponsor.js
@@ -7,6 +7,8 @@ exports.sponsor = asyncHandler(async (req, res) => {
     lastName,
     firstName,
     companyName,
+    businessName,
+    businessDesc,
     email,
     phoneNumber,
     additionalInfo,
@@ -21,10 +23,13 @@ exports.sponsor = asyncHandler(async (req, res) => {
   }
 
   const sponsorHTML = `
-      <h2>New Sponsorship Request</h2>
+      <h2>New Sponsorship/Exhibitor Request</h2>
       <p><strong>Last Name:</strong> ${lastName}</p>
       <p><strong>First Name:</strong> ${firstName}</p>
-      <p><strong>Company Name:</strong> ${companyName || "N/A"}</p>
+      <p><strong>Company Name/Business Name:</strong> ${
+        companyName || businessName || "N/A"
+      }</p>
+      <p><strong>Business Description:</strong> ${businessDesc || "N/A"}</p>
       <p><strong>Email:</strong> ${email}</p>
       <p><strong>Phone Number:</strong> ${phoneNumber}</p>
       <p><strong>Additional Info:</strong> ${additionalInfo || "N/A"}</p>
@@ -33,13 +38,13 @@ exports.sponsor = asyncHandler(async (req, res) => {
   const html = `
     <h2>Hi ${firstName},</h2>
     <p>Thank you for reaching out! We have received your details and will get back to you shortly.</p>
-    <p>Best regards, Sponsor Team</p>`;
+    <p>Best regards, Sponsor/Exhibitor Team</p>`;
 
   await sendConfirmationMail({ html, email, firstName });
   await sendSponsorMail({ html: sponsorHTML });
 
   res.status(200).json({
     success: true,
-    message: "Sponsorship form submitted successfully.",
+    message: "Form submitted successfully.",
   });
 });
